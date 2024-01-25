c60d96748c33386de8dd025525afae379a6e07f0
Marsel151205 <mambetalievmarsel2289@gmail.com>
2024-01-25 14:17:37 +0600

    new push

--- mylibrary/build.gradle.kts
+++ mylibrary/build.gradle.kts
@@ -25,11 +25,11 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
@@ -38,7 +38,7 @@ publishing {
         create<MavenPublication>("mylibrary") {
             groupId = "com.github"
             artifactId = "my"
-            version = "1.0.1"
+            version = "1.0.3"
         }
     }
 }
